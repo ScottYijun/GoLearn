7ac672bbbb7ca35c95286c366fd44fd6e3cac3cb
ScottYijun <chenyijunsz@163.com>
2020-08-01 17:06:51 +0800

    运算符

---
+++ basicTutorial/06operators3.go
@@ -0,0 +1,27 @@
+//06operators3.go
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true
+	var b bool = false
+	if(a && b) {
+		fmt.Printf("第一行 - 条件为 true\n" )
+	}
+	
+	if(a || b) {
+		fmt.Printf("第二行 - 条件为 true\n" )
+	}
+	//修改a 和 b的值 
+	a = false
+	b = true
+	if ( a && b ) {
+		fmt.Printf("第三行 - 条件为 true\n" )
+	} else {
+		fmt.Printf("第三行 - 条件为 false\n" )
+	}
+	if ( !(a && b) ) {
+		fmt.Printf("第四行 - 条件为 true\n" )
+	}
+}

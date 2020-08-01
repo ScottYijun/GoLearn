cc8472a5e986e2cf91276ab723b73e1b50430bb2
ScottYijun <chenyijunsz@163.com>
2020-08-01 11:25:58 +0800

    go语言学习笔记

---
+++ basicTutorial/01test.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	//这是我的第一个go语言程序
+	fmt.Println("this is first go program")
+}
+	
---
+++ basicTutorial/02basicSyntax.go
@@ -0,0 +1,16 @@
+/*
+	filename:02basicSyntax.go
+	date: 2020-09-01
+	brief: go语言基本语法
+*/
+package main
+
+import "fmt"
+
+func main() {
+	//字符串连接
+	fmt.Println("Basic" + "Tutorial")
+	fmt.Println("Basic" + " Tutorial")
+	fmt.Println("Basic\n" + "Tutorial")
+}
+	

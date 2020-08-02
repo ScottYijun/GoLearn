1fabf69138a19e009b86d2fb963f7478421ad60e
ScottYijun <chenyijunsz@163.com>
2020-08-02 17:22:49 +0800

    go语言切片

---
+++ basicTutorial/14slice.go
@@ -0,0 +1,14 @@
+//14slice.go
+package main
+
+import "fmt"
+
+func main() {
+	var numbers = make([]int, 3, 5)
+	
+	printSlice(numbers)
+}
+	
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+}

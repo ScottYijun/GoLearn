db87c2a461de3c758bcd4ab2d354304b26635e60
ScottYijun <chenyijunsz@163.com>
2020-08-02 10:49:12 +0800

    go指针

---
+++ basicTutorial/12pointers.go
@@ -0,0 +1,12 @@
+//12pointers.go
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	fmt.Printf("变量的地址: %x\n", &a)
+}
+
+
+	
---
+++ basicTutorial/12pointers3.go
@@ -0,0 +1,12 @@
+//12pointers3.go
+package main
+
+import "fmt"
+
+func main() {
+	var ptr *int
+	fmt.Printf("ptr 的值为: %x\n", ptr)
+}
+
+
+	
